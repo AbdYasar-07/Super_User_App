d3f14ebc19a6b111d7f68427feeaeb66ef2a7ed1
jeevanandham <112077097+jeevanandhamarjunan@users.noreply.github.com>
2023-07-19 18:54:48 +0530

    Add user changed

--- src/Components/Contents/ContentOutlet.jsx
+++ src/Components/Contents/ContentOutlet.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ContentHeader from "../Contents/ContentHeader";
 import ContentBody from "../Contents/ContentBody";
 import AddUser from "../Users/AddUser";
+import Modal from "../Users/demoModal";
 
 const ContentOutlet = () => {
   const [isUserAdded, setIsUserAdded] = useState(false);
@@ -11,7 +12,7 @@ const ContentOutlet = () => {
         title="Users"
         description="Open a user to add them to a group or assign them to a role"
       />
-      {/* <AddUser setIsUserAdded={setIsUserAdded} /> */}
+      <AddUser setIsUserAdded={setIsUserAdded} />
       <ContentBody isUserAdded={isUserAdded} />
     </div>
   );
--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Axios from "../../Utils/Axios";
+import "../Styles/AddUser.css";
+import { FaUser } from "react-icons/fa";
+
 function AddUser({ setIsUserAdded }) {
   const [userEmail, setUserEmail] = useState("");
   const [userName, setUsername] = useState("");
@@ -10,258 +13,325 @@ function AddUser({ setIsUserAdded }) {
   const [validation, setValidation] = useState(false);
   const [emailValidation, setEmailValidation] = useState(false);
   const [passwordValidation, setPasswordValidation] = useState(false);
+  const [passwordCapableValidation, setPasswordCapableValidation] =
+    useState(false);
+  const [isPassWordValue, setIsPasswordValue] = useState(false);
+  const [repeatPasswordValidation, setRepeatPasswordValidation] =
+    useState(false);
+  const [userModal, setUserModal] = useState(false);
   const initializeFileds = () => {
+    setUserModal(true);
     setValidation(false);
     setEmailValidation(false);
     setPasswordValidation(false);
+    setPasswordCapableValidation(false);
+    setRepeatPasswordValidation(false);
+    setIsPasswordValue(false);
     setUserEmail("");
     setUsername("");
-    setlistOfConnnection(listOfConnnection);
     setUserPassword("");
     setDatabaseConnection("");
     setRepeatPassword("");
   };
-  const getUserToken = () => {
+
+  const getAuthToken = async () => {
     let body = {
-      client_id: "p4sZ4mRoyx3jhkH7AdYU63m7JWxVqjQP",
-      client_secret:
-        "MhvB9WLvhsGa5NNWQaetTaOTTXqx4Nk8phAQ23yY3i5069eDaYu91jpDPIU8rQt0",
-      audience: "https://dev-34chvqyi4i2beker.jp.auth0.com/api/v2/",
-      grant_type: "client_credentials",
+      client_id: process.env.REACT_APP_AUTH_MANAGEMENT_CLIENT_ID,
+      client_secret: process.env.REACT_APP_AUTH_MANAGEMENT_CLIENT_SECRET,
+      audience: process.env.REACT_APP_AUTH_MANAGEMENT_AUDIENCE,
+      grant_type: process.env.REACT_APP_AUTH_GRANT_TYPE,
     };
-    Axios(
+    return await Axios(
       "https://dev-34chvqyi4i2beker.jp.auth0.com/oauth/token",
       "POST",
       body,
       null
     )
-      .then((userToken) => {
-        localStorage.setItem("user_token", userToken.access_token);
-        getAllDataBaseConnection();
+      .then(async (managementToken) => {
+        return managementToken;
       })
       .catch((error) => {
-        console.error("error ::", error);
+        return "Error ::", error;
       });
   };
-  const getAllDataBaseConnection = () => {
-    Axios(
-      "https://dev-34chvqyi4i2beker.jp.auth0.com/api/v2/connections",
-      "GET",
-      null,
-      localStorage.getItem("user_token")
-    )
-      .then((dataBase) => {
-        console.log("database", dataBase);
-        setlistOfConnnection(dataBase);
-      })
-      .catch((error) => {
-        console.error("error ::", error);
-      });
+  const getDatabaseConnections = async () => {
+    // check whether the access_token is valid or not
+    if (localStorage.getItem("access_token")?.length > 0) {
+      await getAuthToken()
+        .then(async (managementToken) => {
+          await Axios(
+            "https://dev-34chvqyi4i2beker.jp.auth0.com/api/v2/connections?strategy=auth0",
+            "GET",
+            null,
+            managementToken?.access_token
+          )
+            .then((databaseNames) => {
+              setlistOfConnnection(databaseNames);
+            })
+            .catch((error) => {
+              console.error("Error while fetching Auth0 Databases ::", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error while fetching mangement token :::", error);
+        });
+    }
   };
-  const addUser = () => {
-    console.log("2callled");
-    let body = {
-      email: userEmail,
-      name: userName,
-      connection: databaseConnection,
-      password: userPassword,
-    };
-    Axios(
-      "https://dev-34chvqyi4i2beker.jp.auth0.com/api/v2/users",
-      "POST",
-      JSON.stringify(body),
-      localStorage.getItem("user_token"),
-      true
-    )
-      .then((addedUser) => {
-        console.log(addedUser, "addedUser");
-        setIsUserAdded(true);
-        initializeFileds();
-      })
-      .catch((error) => {
-        console.error("error ::", error.message);
+
+  const createUser = async () => {
+    // check whether the access_token is valid or not
+    if (localStorage.getItem("access_token")?.length > 0) {
+      await getAuthToken().then(async (managementToken) => {
+        let body = {
+          email: userEmail,
+          name: userName,
+          connection: databaseConnection,
+          password: userPassword,
+        };
+        await Axios(
+          "https://dev-34chvqyi4i2beker.jp.auth0.com/api/v2/users",
+          "POST",
+          JSON.stringify(body),
+          managementToken?.access_token,
+          true
+        )
+          .then((addedUser) => {
+            console.log(addedUser, "addedUser");
+            setIsUserAdded(true);
+            setUserModal(false);
+          })
+          .catch((error) => {
+            console.error("error ::", error.message);
+          });
       });
+    }
   };
-  const getUserData = () => {
-    setValidation(true);
-    userPassword !== repeatPassword
-      ? setPasswordValidation(true)
-      : setPasswordValidation(false);
+  const isemailvalidate = () => {
     let emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
       userEmail
     );
-
     !emailValidation ? setEmailValidation(true) : setEmailValidation(false);
+  };
+
+  const isPassWordValidate = () => {
+    setIsPasswordValue(true);
+    let passwordValidate =
+      /^(?=.*[!@#$%^&*(),.?":{}|<>])[a-zA-Z0-9!@#$%^&*(),.?":{}|<>]{5,}$/.test(
+        userPassword
+      );
+    !passwordValidate
+      ? setPasswordCapableValidation(true)
+      : setPasswordCapableValidation(false);
+  };
+
+  const comparePassword = () => {
+    setRepeatPasswordValidation(true);
+    userPassword !== repeatPassword
+      ? setPasswordValidation(true)
+      : setPasswordValidation(false);
+  };
+
+  const getUserData = () => {
+    setValidation(true);
+    setIsPasswordValue(true);
+    setRepeatPasswordValidation(true);
 
     if (
+      !(
+        repeatPasswordValidation &&
+        isPassWordValue &&
+        passwordCapableValidation &&
+        emailValidation &&
+        passwordValidation &&
+        databaseConnection.length !== 0
+      ) &&
+      userName.length !== 0 &&
       userEmail.length !== 0 &&
       userPassword.length !== 0 &&
       databaseConnection.length !== 0
     ) {
-      addUser();
+      createUser();
     }
   };
   const toggleButton = () => {
     setIsUserAdded(false);
+    setUserModal(false);
   };
   useEffect(() => {
-    getUserToken();
+    const init = () => {
+      getDatabaseConnections();
+    };
+
+    init();
   }, []);
   return (
     <div>
       <button
         type="button"
         class="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-        data-bs-whatever="@mdo"
-        onClick={initializeFileds}
+        onClick={() => initializeFileds()}
       >
         + Create user
       </button>
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Create user
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-                onClick={toggleButton}
-              ></button>
-            </div>
-            <div class="modal-body">
-              <form class="row g-2 needs-validation">
-                <div class="mb-3 text-start">
-                  <label for="recipient-name" class="col-form-label">
-                    Name<span className="text-danger ps-1">*</span>
-                  </label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="userEmail"
-                    value={userName}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                  {validation && !userName && (
-                    <p className="text-danger mt-1 mb-0">Name is required *</p>
-                  )}
-                </div>
-                <div class="mb-3 text-start">
-                  <label for="recipient-name" class="col-form-label">
-                    Email<span className="text-danger ps-1">*</span>
-                  </label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="userEmail"
-                    value={userEmail}
-                    onChange={(e) => setUserEmail(e.target.value)}
-                  />
-                  {validation && !userEmail && (
-                    <p className="text-danger mt-1 mb-0">Email is required *</p>
-                  )}
-                  {emailValidation && (
-                    <p className="text-danger mt-1 mb-0">
-                      Enter valid e-mail *
-                    </p>
-                  )}
-                </div>
-                <div class="mb-3 text-start">
-                  <label for="recipient-name" class="col-form-label">
-                    Password<span className="text-danger ps-1">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="userPassword"
-                    value={userPassword}
-                    onChange={(e) => setUserPassword(e.target.value)}
-                  />
-                  {validation && !userPassword && (
-                    <p className="text-danger mt-1 mb-0">
-                      Password is required
-                    </p>
-                  )}
-                </div>
-                <div class="mb-3 text-start">
-                  <label for="recipient-name" class="col-form-label">
-                    Repeat Password<span className="text-danger ps-1">*</span>
-                  </label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="repeatePassword"
-                    value={repeatPassword}
-                    onChange={(e) => setRepeatPassword(e.target.value)}
-                  />
-                  {validation && !repeatPassword && (
-                    <p className="text-danger mt-1 mb-0">
-                      Repeated Password is required
-                    </p>
-                  )}
-                  {passwordValidation && (
-                    <p className="text-danger">
-                      Password should be same as password
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <label for="conection" className="pe-4 d-block text-start">
-                    Connection <span className="text-danger ms-2">*</span>
-                  </label>
-                  <select
-                    className="w-100 form-control"
-                    onChange={(e) => setDatabaseConnection(e.target.value)}
-                  >
-                    <option value={databaseConnection}> None </option>
-                    {listOfConnnection &&
-                      listOfConnnection?.map((dataBase, index) => {
-                        return (
-                          <option value={dataBase.name} key={index}>
-                            {dataBase.name}
-                          </option>
-                        );
-                      })}
-                    {!databaseConnection && (
-                      <option value={"No data base found"}>
-                        No data base found
-                      </option>
+      {userModal && (
+        <div className="customModal">
+          <div className="col-3 m-auto">
+            <div class="modal-content container col-4">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                  Create User <FaUser style={{ marginBottom: "4px" }} />
+                </h1>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => toggleButton()}
+                ></button>
+              </div>
+              <div class="modal-body">
+                <form class="row g-2 needs-validation">
+                  <div class="mb-3 text-start">
+                    <label for="recipient-name" class="col-form-label">
+                      Name<span className="text-danger ps-1">*</span>
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="userEmail"
+                      value={userName}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                    {validation && !userName && (
+                      <p className="text-danger mt-1 mb-0">
+                        Name is required *
+                      </p>
                     )}
-                  </select>
-                </div>
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-                onClick={toggleButton}
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                class="btn btn-primary"
-                onClick={getUserData}
-              >
-                Create
-              </button>
+                  </div>
+                  <div class="mb-3 text-start">
+                    <label for="recipient-name" class="col-form-label">
+                      Email<span className="text-danger ps-1">*</span>
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="userEmail"
+                      value={userEmail}
+                      onChange={(e) => setUserEmail(e.target.value)}
+                      onBlur={isemailvalidate}
+                    />
+                    {validation && !userEmail && (
+                      <p className="text-danger mt-1 mb-0">
+                        Email is required *
+                      </p>
+                    )}
+                    {userEmail && emailValidation && (
+                      <p className="text-danger mt-1 mb-0">
+                        Enter valid e-mail *
+                      </p>
+                    )}
+                  </div>
+                  <div class="mb-3 text-start">
+                    <label for="recipient-name" class="col-form-label">
+                      Password<span className="text-danger ps-1">*</span>
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="userPassword"
+                      value={userPassword}
+                      onChange={(e) => setUserPassword(e.target.value)}
+                      onBlur={isPassWordValidate}
+                    />
+                    {isPassWordValue && !userPassword && (
+                      <p className="text-danger mt-1 mb-0">
+                        Password is required
+                      </p>
+                    )}
+                    {userPassword &&
+                      passwordCapableValidation &&
+                      isPassWordValue && (
+                        <p className="text-danger">
+                          Password should have atleast 5 characters and special
+                          characters
+                        </p>
+                      )}
+                  </div>
+                  <div class="mb-3 text-start">
+                    <label for="recipient-name" class="col-form-label">
+                      Repeat Password<span className="text-danger ps-1">*</span>
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="repeatePassword"
+                      value={repeatPassword}
+                      onChange={(e) => setRepeatPassword(e.target.value)}
+                      onBlur={comparePassword}
+                    />
+                    {repeatPasswordValidation && !repeatPassword && (
+                      <p className="text-danger mt-1 mb-0">
+                        Repeated Password is required
+                      </p>
+                    )}
+                    {repeatPassword && passwordValidation && (
+                      <p className="text-danger">
+                        Password should be same as password
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <label for="conection" className="pe-4 d-block text-start">
+                      Connection <span className="text-danger ms-2">*</span>
+                    </label>
+                    <select
+                      className="w-100 form-control"
+                      onChange={(e) => setDatabaseConnection(e.target.value)}
+                    >
+                      <option value={"none"}> None </option>
+                      {listOfConnnection.length > 0 &&
+                        listOfConnnection?.map((dataBase, index) => {
+                          return (
+                            <option value={dataBase.name} key={index}>
+                              {dataBase.name}
+                            </option>
+                          );
+                        })}
+                      {listOfConnnection.length == 0 && (
+                        <option value={"No data base found"}>
+                          No data base found
+                        </option>
+                      )}
+                    </select>
+                    {validation && databaseConnection === "none" && (
+                      <p className="text-start text-danger">
+                        Connection is required
+                      </p>
+                    )}
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  onClick={toggleButton}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  onClick={() => getUserData()}
+                >
+                  Create
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
